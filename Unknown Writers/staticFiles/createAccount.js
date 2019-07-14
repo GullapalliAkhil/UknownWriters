36aceb53d2c325e421dbcdcae0962caaa8a41e14
Akhil Gullapalli (3P) <Akhil.Gullapalli@citrix.com>
2019-07-14 19:32:14 +0530

    Front end validation for signup page

--- Unknown Writers/staticFiles/createAccount.js
+++ Unknown Writers/staticFiles/createAccount.js
@@ -17,4 +17,42 @@ function mouseout(){
   ele.style.color="#3b9bf5";
   
 }
+function passwordfieldvalidation() {
+  var x = document.getElementById("pwd_1").value;
+  var y =document.getElementById("pwd_2").value;
+  
+  if (x == "") {
+    document.getElementById("errors").innerHTML="   *Password field cannot be NULL.";
+
+		return false;
+   }
+  if(y == ""){
+    document.getElementById("errors").innerHTML="*Confirm Password field cannot be NULL.";
+
+		return false;
+  
+   }
+if(x !=y){
+
+  document.getElementById("errors").innerHTML="*Password and Confirm Password fields should match.";
+
+  return false;
+  
+   }
+if(x>14){
+
+  document.getElementById("errors").innerHTML="Password characters should be between 5 and 14.";
+
+  return false;
+  
+  }
+if(x<5){
+
+  document.getElementById("errors").innerHTML="Password characters should be between 5 and 14.";
+
+  return false;
+  
+  }
+
+}
 
